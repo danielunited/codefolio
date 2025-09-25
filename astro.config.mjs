c0ed3e687872979377b25f7f42dfe205242c4d28
Dev Andrés Mendoza <mmgamingdev@gmail.com>
2025-09-25 14:15:08 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,17 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+  
+export default defineConfig({
   site: 'https://devandresm.github.io',
-  base: 'mi-repo',
-export default defineConfig({});
+  integrations: [
+  mdx(),
+  sitemap(),
+  tailwind({
+    applyBaseStyles: false
+          })
+    ]
+});
