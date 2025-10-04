5300f28a92c7ce61e96698004c68301b74912054
Ibrahh05 <7464122@alu.murciaeduca.es>
2025-10-04 16:00:29 +0200

    cambio en el site en astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-      site: 'https://Ibrahh05.github.io',
+     site: 'https://Ibrahh05.github.io/codefolio',
 
 });
